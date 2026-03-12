f5fa2de2cddd3d6d1ac36973ccd319edb18d363a
nathanielpierson <nathanielepierson@gmail.com>
2026-03-11 21:07:29 -0400

    add two missing icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import TournamentBracket from './components/TournamentBracket'
-import './App.css'
+import TournamentBracket from "./components/TournamentBracket";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <TournamentBracket />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
